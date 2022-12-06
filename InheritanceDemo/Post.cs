408793cfcb1553682b26fef30d46ea96105f978a
samuelmalik <samuelmalik9>
2022-12-06 09:58:34 +0100

    create Post class, create object from that class and try overrided ToString() method

--- InheritanceDemo/Post.cs
+++ InheritanceDemo/Post.cs
@@ -8,7 +8,7 @@ namespace InheritanceDemo
 {
     class Post
     {
-        private static int currentPostID;
+        private static int currentPostID = 0;
 
         // properties
         protected int ID { get; set; }
@@ -16,7 +16,38 @@ namespace InheritanceDemo
         public string SendByUsername { get; set; }
         public bool IsPublic { get; set; }
 
-        // 
-        
+        // default construcotr
+        public Post()
+        {
+            this.ID = 0;
+            this.Title = "MyfirstPost";
+            this.IsPublic = true;
+            this.SendByUsername = "Oliverko Krška";
+        }
+
+        // instance constructor
+        public Post(string title, bool isPublic, string sendByUsername)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+        }
+
+        protected int GetNextID()
+        {
+            return ++currentPostID;
+        }
+
+        public void Update(string title, bool isPublic)
+        {
+            this.Title = title;
+            this.IsPublic = isPublic;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - by {2}", this.ID, this.Title, this.SendByUsername );
+        }
     }
 }
--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace InheritanceDemo
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace InheritanceDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Oliverko Krška");
+            Console.WriteLine(post1.ToString());
         }
     }
 }
