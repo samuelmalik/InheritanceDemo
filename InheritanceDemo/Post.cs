2824119e43ba5949ad69a3dffff9395ee40c9e99
z21ista20 <z21ista20@z00p1-21.SPSKNM>
2022-12-06 09:38:02 +0100

    Add project files.

---
+++ InheritanceDemo/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    class Post
+    {
+        private static int currentPostID;
+
+        // properties
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        public string SendByUsername { get; set; }
+        public bool IsPublic { get; set; }
+
+        // 
+        
+    }
+}
