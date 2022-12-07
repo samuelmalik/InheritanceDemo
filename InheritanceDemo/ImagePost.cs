67799b14e0eeed424dcc75f521e4f89cbb134361
samuelmalik <samuelmalik9@gmail.com>
2022-12-07 20:38:53 +0100

    add class ImagePost

---
+++ InheritanceDemo/ImagePost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    internal class ImagePost : Post
+    {
+        public string ImageURL { get; set; }
+
+        public ImagePost() { }
+
+        public ImagePost(string title, string sendByUsername,  bool isPublic, string imageURL) : base(title, isPublic, sendByUsername)
+        {
+            this.ID = GetNextID();
+            this.ImageURL = imageURL;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{base.ToString()} - {this.ImageURL}");
+        }
+    }
+}
--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -11,6 +11,8 @@ namespace InheritanceDemo
         {
             Post post1 = new Post("Thanks for the birthday wishes", true, "Oliverko Krška");
             Console.WriteLine(post1.ToString());
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Oliverko Krška", true, "https://images.com/shoes");
+            Console.WriteLine(imagePost1.ToString());
         }
     }
 }
